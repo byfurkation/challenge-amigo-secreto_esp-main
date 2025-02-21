43475808d01c2be0634928cb127a88f3a95a0e4e
byfurkation <byfurkation@gmail.com>
2025-02-21 00:32:52 -0600

    actualizacion funcion agregar amigos

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
-let ListaAmigos = [];
+let amigos = [];
 let numeroMaximo = 10;
 let numeroMinimo = 3;
 
 
 //Función para agregar los amigos
 function agregarAmigo() {
-    let amigo = document.getElementById('amigo').value;
+    const amigo = document.getElementById('amigo').value;
      //validamos que el nombre no esté vacío
      if (amigo === "") {
-        alert("Debes ingresar un nombre por favor");
+        alert("Por favor, inserte un nombre.");
         return; //para que no siga ejecutando el código
-    } else { 
-        ListaAmigos.push(amigo); //agregamos el amigo a la lista
-        document.getElementById('amigo').value = ""; //limpiamos el input
-        console.log(ListaAmigos); //imprimimos la lista de amigos
+    } else { //si el nombre no está vacío
+        amigos.push(amigo); //agregamos el amigo a la lista
+        document.getElementById('amigo').value = ""; //limpiamos la caja de texto
+        console.log(amigos); //imprimimos la lista de amigos
         console.log(typeof(amigo)); //imprimimos el tipo de dato de amigo
-        console.log(ListaAmigos.length); //imprimimos la longitud de la lista de amigos
+        console.log(amigos.length); //imprimimos la longitud de la lista de amigos
     }
 }
 
-/*    
-function agregarAmigo() {
-    // Obtenemos el nombre del amigo
-    let nombre = document.getElementById('amigo').value;
-    
-    //validamos que el nombre no esté vacío
-    if (nombre === "") {
-        alert("Debes ingresar un nombre por favor");
-        return; //para que no siga ejecutando el código
-    }
+// función para actualizar la lista de amigos en la interfaz
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; //limpiamos la lista de amigos
+    listaAmigos.forEach((amigo) => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
 
+
+
+/*
+//función para seleccionar un amigo de la lista al azar
+function seleccionarAmigo() {
+    let amigoSeleccionado = ListaAmigos[Math.floor(Math.random() * ListaAmigos.length)];
+    document.getElementById('amigoSeleccionado').textContent = amigoSeleccionado;
+}
+   
+
+ 
     //validamos que el número de amigos no sea mayor al número máximo
     if (amigos.length >= numeroMaximo) {
         alert("No puedes agregar más amigos");
